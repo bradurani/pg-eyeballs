a16898f8972c47e5cab4d5f2832e1e2985c8fe53
Brad Urani <bradurani@gmail.com>
2016-09-14 22:58:42 -0700

    Empty gem

---
+++ lib/pg/eyeballs.rb
@@ -0,0 +1,7 @@
+require "pg/eyeballs/version"
+
+module Pg
+  module Eyeballs
+    # Your code goes here...
+  end
+end
---
+++ lib/pg/eyeballs/version.rb
@@ -0,0 +1,5 @@
+module Pg
+  module Eyeballs
+    VERSION = "0.1.0"
+  end
+end
